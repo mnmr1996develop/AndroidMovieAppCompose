73d13c4da76ef511473c15b57468446e0a3ca369
mnmr1996develop <93550664+mnmr1996develop@users.noreply.github.com>
2023-04-06 13:58:59 -0400

    working on the main widget

--- app/src/main/java/com/michaelrichards/movieapp/widgets/widgets.kt
+++ app/src/main/java/com/michaelrichards/movieapp/widgets/widgets.kt
@@ -1,41 +1,67 @@
 package com.michaelrichards.movieapp.widgets
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
+import coil.transform.CircleCropTransformation
 import com.michaelrichards.movieapp.model.Movie
+import com.michaelrichards.movieapp.model.getMovies
 
+@Preview
 @Composable
-fun MovieRow(movie: Movie, onItemClick: (String) -> Unit = {}){
-    Card(modifier = Modifier
-        .padding(4.dp)
-        .fillMaxWidth()
-        .height(130.dp)
-        .clickable {
-            onItemClick(movie.id)
-        },
-        shape = RoundedCornerShape(corner = CornerSize(20.dp))
-        , elevation = 8.dp) {
-        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Start) {
-            Surface(modifier = Modifier
-                .padding(12.dp)
-                .size(100.dp), shape = RectangleShape, elevation = 4.dp) {
-                Icon(imageVector = Icons.Default.AccountBox , contentDescription = "$movie Image")
+fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp)
+            .clickable {
+                onItemClick(movie.id)
+            },
+        shape = RoundedCornerShape(corner = CornerSize(20.dp)), elevation = 8.dp
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Start
+        ) {
+            Surface(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(100.dp), shape = RectangleShape, elevation = 4.dp
+            ) {
+                Image(
+                    painter = rememberAsyncImagePainter(
+                        ImageRequest.Builder(LocalContext.current).data(data = movie.images[0])
+                            .apply(block = fun ImageRequest.Builder.() {
+                                crossfade(true)
+                                transformations(CircleCropTransformation())
+                            }).build()
+                    ), contentDescription = "${movie.title} image poster"
+                )
 
             }
-            Text(text = movie.title)
+            Column(modifier = Modifier.padding(5.dp)) {
+                Text(text = movie.title, style = MaterialTheme.typography.h6)
+                Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.caption)
+                Text(text = "Released: ${movie.year}")
+            }
+
         }
     }
 }
