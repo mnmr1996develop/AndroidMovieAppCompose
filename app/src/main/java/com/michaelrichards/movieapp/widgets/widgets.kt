c3fc29d614d60c2dc1d8d7ba8f72279bb777af71
mnmr1996develop <93550664+mnmr1996develop@users.noreply.github.com>
2023-04-06 14:43:28 -0400

    finishing off the widget

--- app/src/main/java/com/michaelrichards/movieapp/widgets/widgets.kt
+++ app/src/main/java/com/michaelrichards/movieapp/widgets/widgets.kt
@@ -1,5 +1,6 @@
 package com.michaelrichards.movieapp.widgets
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
@@ -9,13 +10,21 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
 import coil.request.ImageRequest
@@ -26,11 +35,15 @@ import com.michaelrichards.movieapp.model.getMovies
 @Preview
 @Composable
 fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) {
+
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
     Card(
         modifier = Modifier
             .padding(4.dp)
             .fillMaxWidth()
-            .height(130.dp)
             .clickable {
                 onItemClick(movie.id)
             },
@@ -60,8 +73,35 @@ fun MovieRow(movie: Movie = getMovies()[0], onItemClick: (String) -> Unit = {}) 
                 Text(text = movie.title, style = MaterialTheme.typography.h6)
                 Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.caption)
                 Text(text = "Released: ${movie.year}")
+                Icon(imageVector = if (expanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown, contentDescription = "Down Arrow", modifier = Modifier
+                    .size(30.dp)
+                    .clickable {
+                        expanded = !expanded
+                    })
+
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+                        Text(buildAnnotatedString {
+                            withStyle(style = SpanStyle(color = Color.DarkGray, fontSize = 13.sp)){
+                                append("Plot: ")
+                            }
+                            withStyle(style = SpanStyle(color = Color.Black, fontSize = 13.sp, fontWeight = FontWeight.Light)){
+                                append(movie.plot)
+                            }
+                        }, modifier = Modifier.padding(6.dp))
+
+                        Divider()
+                        Text(text = "Director: ${movie.director}", style = MaterialTheme.typography.caption)
+                        Text(text = "Actors: ${movie.actors}", style = MaterialTheme.typography.caption)
+                        Text(text = "Rating: ${movie.rating}", style = MaterialTheme.typography.caption)
+                    }
+                }
             }
 
         }
     }
+
+
+
+
 }
